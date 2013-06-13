488fc76abb88818e78a21ffa6b079a9fb280488a
csra.projects@gmail.com <csra.projects@gmail.com@dd94caea-49c3-3581-2987-14f8d5a49f03>
2013-06-13 04:40:32 +0000

    Kinda working swip

--- trunk/BUScience/src/com/example/buscience/HomeActivity.java
+++ trunk/BUScience/src/com/example/buscience/HomeActivity.java
@@ -8,6 +8,11 @@ import android.widget.*;
 
 public class HomeActivity extends Activity
 {
+    private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int SWIPE_MAX_OFF_PATH = 100;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 100;
+    private GestureDetector myGestureDetector;
+	
     public void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
@@ -27,17 +32,50 @@ public class HomeActivity extends Activity
             image.setBackgroundResource(slideImgs[i]);
             flipper.addView(image);
         }        
-        flipper.startFlipping();
+        //flipper.startFlipping();
 
-        flipper.setOnClickListener(new OnClickListener() 
+        myGestureDetector = new GestureDetector(new MySwipeDetector());
+        flipper.setOnTouchListener(new View.OnTouchListener() 
         {
-            public void onClick(View v) 
-            {
-            	ViewFlipper flipper = ((ViewFlipper)findViewById(R.id.viewFlipper1));
-            	flipper.stopFlipping();
-            	flipper.showNext();
-            	flipper.startFlipping();
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				return myGestureDetector.onTouchEvent(event);
+			}
+		});
+        
+//        flipper.setOnClickListener(new OnClickListener() 
+//        {
+//            public void onClick(View v) 
+//            {
+//            	ViewFlipper flipper = ((ViewFlipper)findViewById(R.id.viewFlipper1));
+//            	flipper.stopFlipping();
+//            	flipper.showNext();
+//            	flipper.startFlipping();
+//            }
+//        });
+    }
+    
+    class MySwipeDetector extends GestureDetector.SimpleOnGestureListener
+    {
+    	@Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY)
+    	{
+    		try {
+                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+                    return false;
+                
+                ViewFlipper flipper = ((ViewFlipper)findViewById(R.id.viewFlipper1));
+                flipper.stopFlipping();
+                if(e1.getX() - e2.getX() >= SWIPE_MIN_DISTANCE && Math.abs(velocityX) >= SWIPE_THRESHOLD_VELOCITY) {
+                    flipper.showPrevious();
+                }  else if (e2.getX() - e1.getX() >= SWIPE_MIN_DISTANCE && Math.abs(velocityX) >= SWIPE_THRESHOLD_VELOCITY) {
+                	flipper.showNext();
+                }
+                flipper.startFlipping();
+            } catch (Exception e) {
+                // Do Nothing
             }
-        });
+            return true;
+    	}
     }
 }
