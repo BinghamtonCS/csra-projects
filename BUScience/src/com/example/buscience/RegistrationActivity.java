b7dd70ef98f28c53790b59088a91c0c34dacdf23
csra.projects@gmail.com <csra.projects@gmail.com@dd94caea-49c3-3581-2987-14f8d5a49f03>
2013-08-23 16:47:05 +0000

    School Teacher Sign Up Page Version 1

--- BUScience/src/com/example/buscience/RegistrationActivity.java
+++ BUScience/src/com/example/buscience/RegistrationActivity.java
@@ -1,25 +1,32 @@
 package com.example.buscience;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.*;
 
-public class RegistrationActivity extends Activity
+public class RegistrationActivity extends Activity 
+	implements TabHost.OnTabChangeListener
 {
+	private TabHost schoolTabs;
+	
     public void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_layout);
     }
     
-    public void schoolTeacherClicked(View view)
+	@SuppressLint("NewApi")
+	public void schoolTeacherClicked(View view)
     {
     	setContentView(R.layout.school_teacher_layout);
 		MainActivity.setTitle("School-Teacher Sign-Up");
@@ -31,12 +38,40 @@ public class RegistrationActivity extends Activity
 		browserTimeSlot.setInitialScale(165);
 		browserTimeSlot.loadUrl("https://spreadsheets.google.com/spreadsheet/ccc?chrome=false&key=0AphPhvpO0nOPdDRHNnVneW5NTjdzSzdJVTYxWndoSXc&output=html&pubredirect=true&widget=true");
 
+		schoolTabs = (TabHost)findViewById(R.id.schoolTabs);
+		schoolTabs.setup();
+		addTab(schoolTabs, "Sign-Up", R.id.tabSchoolSignUp);
+		addTab(schoolTabs, "Schedule Change", R.id.tabSchoolUpdate);
+		
+		setTabBackground(schoolTabs.getCurrentTab());
+		schoolTabs.setOnTabChangedListener(this);
+		
+		final WebView browserSchoolSignUp = (WebView)findViewById(R.id.schoolSignUp);
+		browserSchoolSignUp.setWebViewClient(new WebViewClient());
+		browserSchoolSignUp.getSettings().setBuiltInZoomControls(true);
+		browserSchoolSignUp.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+		browserSchoolSignUp.setScrollbarFadingEnabled(false);
+		browserSchoolSignUp.getSettings().setJavaScriptEnabled(true);
+		browserSchoolSignUp.setInitialScale(165);
+		browserSchoolSignUp.loadUrl("https://docs.google.com/spreadsheet/embeddedform?bc=transparent&f=%2522Georgia%2522%252C%2Bserif&formkey=dHI3Ym94WXFFUmJfdE9HSUdQSENzenc6MQ&hl=en&htc=%2523666666&lc=%2523135355&pli=1&tc=%2523565555&ttl=0");
+		
+		final WebView browserSchoolUpdate = (WebView)findViewById(R.id.schoolUpdate);
+		browserSchoolUpdate.setWebViewClient(new WebViewClient());
+		browserSchoolSignUp.getSettings().setBuiltInZoomControls(true);
+		browserSchoolSignUp.setScrollBarStyle(WebView.SCROLLBARS_INSIDE_OVERLAY);
+		browserSchoolSignUp.setScrollbarFadingEnabled(false);
+		browserSchoolUpdate.getSettings().setJavaScriptEnabled(true);
+		browserSchoolUpdate.setInitialScale(165);
+		browserSchoolUpdate.loadUrl("https://docs.google.com/spreadsheet/embeddedform?bc=transparent&f=%2522Georgia%2522%252C%2Bserif&formkey=dDQ3Tk5PcjNYX1plb1RSbnVhOWh2bUE6MQ&hl=en&htc=%2523666666&lc=%2523135355&pli=1&tc=%2523565555&ttl=0");
+
 		final Button clearButton = MainActivity.getClearButton();
 		clearButton.setVisibility(View.VISIBLE);
 		clearButton.setOnClickListener(new OnClickListener()
 		{
 			public void onClick(View v) {
 				browserTimeSlot.loadUrl("https://spreadsheets.google.com/spreadsheet/ccc?chrome=false&key=0AphPhvpO0nOPdDRHNnVneW5NTjdzSzdJVTYxWndoSXc&output=html&pubredirect=true&widget=true");
+				browserSchoolSignUp.loadUrl("https://docs.google.com/spreadsheet/embeddedform?bc=transparent&f=%2522Georgia%2522%252C%2Bserif&formkey=dHI3Ym94WXFFUmJfdE9HSUdQSENzenc6MQ&hl=en&htc=%2523666666&lc=%2523135355&pli=1&tc=%2523565555&ttl=0");
+				browserSchoolUpdate.loadUrl("https://docs.google.com/spreadsheet/embeddedform?bc=transparent&f=%2522Georgia%2522%252C%2Bserif&formkey=dDQ3Tk5PcjNYX1plb1RSbnVhOWh2bUE6MQ&hl=en&htc=%2523666666&lc=%2523135355&pli=1&tc=%2523565555&ttl=0");				
 			}
 		});
 		
@@ -62,4 +97,29 @@ public class RegistrationActivity extends Activity
     {
     	
     }
+    
+    private void addTab(TabHost tabHost, String label, int id) 
+    {
+		TabHost.TabSpec spec = tabHost.newTabSpec(label);
+		View tabIndicator = LayoutInflater.from(this).inflate(R.layout.tab_indicator_reg, tabHost.getTabWidget(), false);
+		TextView title = (TextView)tabIndicator.findViewById(R.id.title);
+		title.setText(label);
+		title.setTextSize(16f);
+		spec.setIndicator(tabIndicator);
+		spec.setContent(id);
+		tabHost.addTab(spec);
+    }
+
+	@Override
+	public void onTabChanged(String arg0) {
+		setTabBackground(schoolTabs.getCurrentTab());
+	}
+	
+	private void setTabBackground(int index) 
+	{
+		for(int i = 0; i < schoolTabs.getTabWidget().getChildCount();i++) {
+			schoolTabs.getTabWidget().getChildAt(i).setBackgroundColor(Color.parseColor("#000000")); //unselected
+	    }
+		schoolTabs.getTabWidget().getChildAt(index).setBackgroundColor(Color.parseColor("#808080")); // selected
+	}
 }
